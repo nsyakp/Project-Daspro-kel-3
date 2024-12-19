8706b573f3a6134cc629d3a70fce73ad90399c41
acera <acera@LAPTOP-J1PMRI0H>
2024-12-19 10:58:47 +0700

    menambahkan biodata

---
+++ src/latihan1/Latihan1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan1;
+
+/**
+ *
+ * @author acera
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        loginView lv = new loginView();
+        lv.setLocationRelativeTo(null);
+        lv.setVisible(true);
+    }
+    
+}
